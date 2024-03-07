813d95262f0894769b73a2ad9af9ea524e1c4ce1
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2024-03-07 16:24:16 -0600

    DB: Configuracion inicial

---
+++ back/classes/Server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const color = require('colors/safe');
+const http = require('http');
+const cors = require('cors');
+const socketIo = require('socket.io');
+const fileUpload = require("express-fileupload");
+require('dotenv').config();
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 4000;
+        this.server = http.createServer( this.app );
+        this.io = socketIo(this.server);
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(){
+        this.app.use( cors() );
+        this.app.use( express.json() );
+        this.app.use( fileUpload(
+            {
+                useTempFiles: true,
+                limits: {fileSize: 50 * 2024 * 1024}
+            }
+        ));
+    }
+
+    routes() {
+        // Index
+        this.app.get('/', (req, res) => {
+            res.send('Backend');
+        });
+    }
+
+    start(){
+        this.server.listen( this.port, () => {
+            console.clear();
+            console.log( color.cyan('Server launched on port:', this.port) );
+        });
+    }
+
+}
+
+module.exports = Server;
